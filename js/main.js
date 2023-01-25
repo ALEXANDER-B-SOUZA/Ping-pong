3782eed84006ee5e83de0a77bcaf38e5ae802446
Alexander-Souza <bsalex12010@gmail.com>
2023-01-25 20:16:48 -0300

    Alterando configuração da mesa.

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 /* declarando variáveis*/
-const canvasEl = document.querySelector("canvas")
-const canvasCtx = canvasEl.getContext("2d")
+const canvasEl = document.getElementById("canvas");
+const Ctx = canvasEl.getContext("2d");
 
 
 
 function setup(){
   canvasEl.width = window.innerWidth
   canvasEl.height = window.innerHeight
-  canvasCtx.width = window.innerWidth
-  canvasCtx.height = window.innerHeight
+  Ctx.width = window.innerWidth
+  Ctx.height = window.innerHeight
 }
 
 function draw(){
-    canvasCtx.fillStyle = "#286047"
-    canvasCtx.fillRect (0, 0, window.innerWidth, window.innerHeight)
+    Ctx.fillStyle = 'blue';
+    Ctx.fillRect (0, 0, window.innerWidth, window.innerHeight)
 }
 
 setup()
