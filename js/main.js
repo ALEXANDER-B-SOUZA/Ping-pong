bb710dd2a7005c1b4f4e28ca630d02b6f532495c
Alexander-Souza <bsalex12010@gmail.com>
2023-01-25 20:43:17 -0300

    Criando raquetes.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 const canvasEl = document.getElementById("canvas");
 const Ctx = canvasEl.getContext("2d");
 const espessura = 15;
-
+const raquete = 10;
 
 function setup(){
   canvasEl.width = window.innerWidth
@@ -12,11 +12,18 @@ function setup(){
 }
 
 function draw(){
+    /* Criando a mesa.*/
     Ctx.fillStyle = 'blue';
     Ctx.fillRect (0, 0, window.innerWidth, window.innerHeight);
-
+    /*Criando a linha do meio da mesa.*/
     Ctx.fillStyle = 'white';
     Ctx.fillRect (window.innerWidth / 2 - espessura / 2, 0, espessura, window.innerHeight);
+    /* Criando raquete direita.*/
+    Ctx.fillStyle = 'white';
+    Ctx.fillRect(raquete, 300, espessura, 200);
+    /*Criando raquete esquerda*/
+    Ctx.fillStyle = 'white';
+    Ctx.fillRect(window.innerWidth - espessura - raquete, 300, espessura, 200);
 }
 
 setup()
