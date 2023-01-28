640ad8e38a6d5ce01f473cf6ef08ab4b409f214c
Alexander-Souza <bsalex12010@gmail.com>
2023-01-28 14:47:55 -0300

    Melhorias.

--- js/main.js
+++ js/main.js
@@ -4,56 +4,56 @@ const Ctx = canvasEl.getContext("2d");
 const espessura = 15;
 const raquete = 10;
 
+/* Criando a mesa.*/
 const campo = {
   w: window.innerWidth,
   h: window.innerHeight,
   draw: function() {
-/* Criando a mesa.*/
     Ctx.fillStyle = 'blue';
     Ctx.fillRect (0, 0, this.w , this.h);
   } ,
 }
 
+/*Criando a linha do meio da mesa.*/
 const linha = {
   w: 15,
   h: campo.h,
-  draw: function() {
- /*Criando a linha do meio da mesa.*/
+  draw: function() { 
     Ctx.fillStyle = 'white';
     Ctx.fillRect (campo.w / 2 - this.w / 2, 0, this.w, this.h);
   } ,
 }
 
+/* Criando raquete direita.*/
 const raqdireita = {
  x: raquete,
  y: 300,
  w: linha.w,
  h: 200,
 draw: function() {
-/* Criando raquete direita.*/
     Ctx.fillStyle = 'white';
     Ctx.fillRect(this.x, this.y, this.w, this.h);
   } ,
 }
 
+/*Criando raquete esquerda*/
 const raqesquerda = {
  x: campo.w -linha.w - espessura,
  y: 300,
  w: linha.w,
  h: 200,
 draw: function() {
-/*Criando raquete esquerda*/
     Ctx.fillStyle = 'white';
     Ctx.fillRect(this.x, this.y, this.w, this.h);
   } ,
 }
 
+/*Criando a bola.*/
 const bola = {
  x: 400,
  y: 240,
  r: 25,
 draw: function() {
-/*Criando a bola.*/
     Ctx.fillStyle = 'white';
     Ctx.beginPath()
     Ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI, false)
